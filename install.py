5ca35e9abc739f7a74eaceb34056a97cfaa9004e
Fatbrother <kevin92422@gmail.com>
2022-12-09 16:36:03 +0800

    installer

---
+++ install.py
@@ -0,0 +1,137 @@
+import os
+
+mingwUrl = "https://github.com/fatbrother/mingw64/archive/refs/heads/main.zip"
+sfmlUrl = "https://www.sfml-dev.org/files/SFML-2.5.1-windows-gcc-7.3.0-mingw-64-bit.zip"
+c_cpp_properties = """
+{
+    "configurations": [
+        {
+            "name": "Win32",
+            "includePath": [
+                "${workspaceFolder}/**",
+                "C:/mingw64/include/c++",
+                "C:/SFML-2.5.1/include"
+            ],
+            "defines": [
+                "_DEBUG",
+                "UNICODE",
+                "_UNICODE"
+            ],
+            "compilerPath": "C:/mingw64/bin/g++.exe",
+            "cStandard": "gnu17",
+            "cppStandard": "gnu++20",
+            "intelliSenseMode": "windows-gcc-x64"
+        }
+    ],
+    "version": 4
+}
+"""
+
+tasks = """
+{
+    "version": "2.0.0",
+    "tasks": [
+        {
+            "type": "cppbuild",
+            "label": "C++: 建置使用中檔案",
+            "command": "C:/mingw64/bin/g++.exe",
+            "args": [
+                "-g",
+                "${fileDirname}/**.cpp",
+                "-o",
+                "${fileDirname}/${fileBasenameNoExtension}.exe",
+                "-Wall",
+                "-Wextra",
+                "-Wpedantic",
+                "-IC:/SFML-2.5.1/include",
+                "-LC:/SFML-2.5.1/lib",
+                "-lsfml-graphics",
+                "-lsfml-audio",
+                "-lsfml-window",
+                "-lsfml-system",
+            ],
+            "options": {
+                "cwd": "C:/Program Files/mingw64/bin"
+            },
+            "problemMatcher": [
+                "$gcc"
+            ],
+            "group": "build",
+            "detail": "編譯器: \"C:/mingw64/g++.exe\""
+        }
+    ]
+}
+"""
+
+testCode = """
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
+"""
+
+dllList = [
+    "C:/mingw64/lib/libgcc_s_seh-1.dll",
+    "C:/mingw64/lib/libstdc++-6.dll",
+    "C:/mingw64/lib/libwinpthread-1.dll",
+    "C:/SFML-2.5.1/bin/openal32.dll",
+    "C:/SFML-2.5.1/bin/sfml-audio-2.dll",
+    "C:/SFML-2.5.1/bin/sfml-graphics-2.dll",
+    "C:/SFML-2.5.1/bin/sfml-network-2.dll",
+    "C:/SFML-2.5.1/bin/sfml-system-2.dll",
+    "C:/SFML-2.5.1/bin/sfml-window-2.dll",
+]
+
+def install():
+    print("Installing SFML...")
+    os.system("wget -O sfml.zip " + sfmlUrl)
+    os.system("unzip sfml.zip")
+    # move SFML to C:/
+    os.system("mv SFML-2.5.1 C:/")
+    print("Installing MinGW...")
+    os.system("wget -O mingw.zip " + mingwUrl)
+    os.system("unzip mingw.zip")
+    # move MinGW to C:/
+    os.system("mv mingw64-main C:/mingw64")
+    
+
+def setup():
+    print("Setting up...")
+    # create c_cpp_properties.json
+    with open(".vscode/c_cpp_properties.json", "w") as f:
+        f.write(c_cpp_properties)
+    # create tasks.json
+    with open(".vscode/tasks.json", "w") as f:
+        f.write(tasks)
+
+    # copy dlls
+    for dll in dllList:
+        os.system("cp " + dll + " .")
+
+    # create test.cpp
+    with open("test.cpp", "w") as f:
+        f.write(testCode)
+
+if __name__ == "__main__":
+    install()
+    setup()
